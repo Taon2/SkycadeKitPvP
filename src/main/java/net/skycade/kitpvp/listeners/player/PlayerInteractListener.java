486797f4dd085dd017ae6d80ff76da5459b1e7ba
Taon2 <taongames@gmail.com>
2019-02-28 22:00:51 -0600

    3.5.3 - Minor bugfix

--- src/main/java/net/skycade/kitpvp/listeners/player/PlayerInteractListener.java
+++ src/main/java/net/skycade/kitpvp/listeners/player/PlayerInteractListener.java
@@ -68,17 +68,22 @@ public class PlayerInteractListener implements Listener {
         if (e.getItem().getType() == Material.MUSHROOM_SOUP) {
             double maxHealth = p.getMaxHealth();
             if (p.getHealth() < maxHealth) {
-                final int heldItemSlot = e.getPlayer().getInventory().getHeldItemSlot();
-                e.getPlayer().getInventory().clear(heldItemSlot);
-                p.updateInventory();
-                addBowl(p);
-
                 if (p.getHealth() < maxHealth - 7) {
                     p.setHealth(p.getHealth() + 7);
                 } else {
                     p.setHealth(maxHealth);
                 }
                 //e.getPlayer().getInventory().setItem(e.getPlayer().getInventory().getHeldItemSlot(), new ItemStack(Material.AIR, 1));
+                final int heldItemSlot = e.getPlayer().getInventory().getHeldItemSlot();
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        e.getPlayer().getInventory().clear(heldItemSlot);
+                        p.updateInventory();
+                        addBowl(p);
+                    }
+                }.runTaskLater(plugin, 1L);
+
             }
             return;
         }
